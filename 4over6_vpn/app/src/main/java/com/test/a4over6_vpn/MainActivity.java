eeea770970991ea89d338d791e17aeea5dbe9243
weifff <1527752351@qq.com>
2017-05-09 14:20:11 +0800

    折线图

--- 4over6_vpn/app/src/main/java/com/test/a4over6_vpn/MainActivity.java
+++ 4over6_vpn/app/src/main/java/com/test/a4over6_vpn/MainActivity.java
@@ -32,7 +32,22 @@ import java.net.InetAddress;
 import java.net.Inet6Address;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
+import lecho.lib.hellocharts.model.Column;
+import lecho.lib.hellocharts.model.ColumnChartData;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.SubcolumnValue;
+import lecho.lib.hellocharts.model.ValueShape;
+import lecho.lib.hellocharts.util.ChartUtils;
+import lecho.lib.hellocharts.view.ColumnChartView;
+import lecho.lib.hellocharts.view.LineChartView;
 
 public class MainActivity extends AppCompatActivity {
     private static String[] info;
@@ -53,12 +68,26 @@ public class MainActivity extends AppCompatActivity {
     private static String downloadTotalPkg;
     private static String uploadTotalLength;
     private static String downloadTotalLength;
+    private static final  int INFOLENGTH = 6;
+    private static int infoKey = 0;
+    private static float uploadSpeedInfo[];
+    private static float downloadSpeedInfo[];
+    private static float uploadTotalPkgInfo[];
+    private static float downloadTotalPkgInfo[];
+    private static float uploadTotalLengthInfo[];
+    private static float downloadTotalLengthInfo[];
+
 
     private boolean IPflag =  true;
 
     private static final int MSG_UPDATEUI = 0;
     private static final int MSG_CONNECT  = 1;
     private static final int MSG_STATUS   = 2;
+
+    private ColumnChartView chart;
+
+    public final static String[] infoLabels = new String[] { "uploadSpeed", "downloadSpeed",
+            "uploadTotalLength", "uploadTotalPkg", "downloadTotalLength", "downloadTotalPkg" };
 
     File infoTunnel;
     FileInputStream infoInputStream;
@@ -70,34 +99,162 @@ public class MainActivity extends AppCompatActivity {
     private Handler mHandler = new Handler() {
         public void handleMessage (Message msg) { // in main(UI) thread
             TextView editText = (TextView) findViewById(R.id.editText);
-            TextView uploadspeed = (TextView)findViewById(R.id.UploadSpeed);
-            TextView downloadspeed = (TextView)findViewById(R.id.DownloadSpeed);
-            TextView uploadLength = (TextView)findViewById(R.id.UploadTotalLength);
-            TextView downloadLength = (TextView)findViewById(R.id.DownloadTotalLength);
-            TextView uploadPkg = (TextView)findViewById(R.id.UploadPkg);
-            TextView downloadPkg = (TextView)findViewById(R.id.DownloadPkg);
+//            TextView uploadspeed = (TextView)findViewById(R.id.UploadSpeed);
+//            TextView downloadspeed = (TextView)findViewById(R.id.DownloadSpeed);
+//            TextView uploadLength = (TextView)findViewById(R.id.UploadTotalLength);
+//            TextView downloadLength = (TextView)findViewById(R.id.DownloadTotalLength);
+//            TextView uploadPkg = (TextView)findViewById(R.id.UploadPkg);
+//            TextView downloadPkg = (TextView)findViewById(R.id.DownloadPkg);
             switch (msg.what) {
                 case MSG_UPDATEUI:
                     Log.d("wjf", "MSG_UPDATEUI");
                     //Done 根据管道数据更新上传速度等信息
                     String update = (String)msg.obj;
                     Log.d("wjf", update);
-                    info = update.split("");
+                    info = update.split(" ");
+                    Log.d("wjf","wjf");
                     if (info.length == 6){
+                        Log.d("wjf","split 6");
+
                         uploadSpeed = info[0];
+                        float tempfloat[] = new float[6];
+
+                        for(int i=0;i<INFOLENGTH-1;i++)
+                        {
+                           uploadSpeedInfo[INFOLENGTH-1-i] = uploadSpeedInfo[INFOLENGTH-1-i-1];
+                            Log.d("wjf","up"+uploadSpeedInfo[i]);
+                        }
+                        uploadSpeedInfo[0] = Float.valueOf(info[0]);
                         downloadSpeed = info[1];
+                        for(int i=0;i<INFOLENGTH-1;i++)
+                        {
+                            downloadSpeedInfo[INFOLENGTH-1-i] = downloadSpeedInfo[INFOLENGTH-1-i-1];
+                        }
+                        downloadSpeedInfo[0] = Float.valueOf(info[1]);
                         uploadTotalLength = info[2];
+                        for(int i=0;i<INFOLENGTH-1;i++)
+                        {
+                            uploadTotalLengthInfo[INFOLENGTH-1-i] = uploadTotalLengthInfo[INFOLENGTH-1-i-1];
+                        }
+                        uploadTotalLengthInfo[0] = Float.valueOf(info[2]);
                         uploadTotalPkg = info[3];
+                        for(int i=0;i<INFOLENGTH-1;i++)
+                        {
+                            uploadTotalPkgInfo[INFOLENGTH-1-i] = uploadTotalPkgInfo[INFOLENGTH-1-i-1];
+                        }
+                        uploadTotalPkgInfo[0] = Float.valueOf(info[3]);
                         downloadTotalLength = info[4];
+                        for(int i=0;i<INFOLENGTH-1;i++)
+                        {
+                            downloadTotalLengthInfo[INFOLENGTH-1-i] = downloadTotalLengthInfo[INFOLENGTH-1-i-1];
+                        }
+                        downloadTotalLengthInfo[0] = Float.valueOf(info[4]);
                         downloadTotalPkg = info[5];
+                        for(int i=0;i<INFOLENGTH-1;i++)
+                        {
+                            downloadTotalPkgInfo[INFOLENGTH-1-i] = downloadTotalPkgInfo[INFOLENGTH-1-i-1];
+                        }
+                        downloadTotalPkgInfo[0] = Float.valueOf(info[5]);
+
                     }
+                    //柱状图++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//                    int numSubcolumns = 1;
+//                    int numColumns = 6;
+//                    List<Column> columns = new ArrayList<Column>();
+//                    List<SubcolumnValue> values;
+//                    List<AxisValue> axisValues = new ArrayList<AxisValue>();
+//                    for (int i = 0; i < numColumns; ++i) {
+//
+//                        values = new ArrayList<SubcolumnValue>();
+//                        for (int j = 0; j < numSubcolumns; ++j) {
+//                            values.add(new SubcolumnValue(
+//                                    Float.valueOf(info[i]),ChartUtils
+//                                    .pickColor()));
+//                        }
+//
+//                        Column column = new Column(values);
+//                        column.setHasLabels(true);
+//
+//                        column.setHasLabelsOnlyForSelected(true);
+//                        columns.add(column);
+//                        axisValues.add(new AxisValue(i).setLabel(infoLabels[i]));
+//                    }
+//
+//                    data = new ColumnChartData(columns);
+//                    Axis axisX = new Axis(axisValues);
+//                    Axis axisY = new Axis().setHasLines(true);
+//                    if (true) {
+//                        axisX.setName("Axis Xwjf");
+//                        axisY.setName("Axis Ywjf");
+//                    }
+//                    data.setAxisXBottom(axisX);
+//                    data.setAxisYLeft(axisY);
+//
+//                    chart.setColumnChartData(data);
+
+                    //柱状图+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+                    //第一条折线+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                    drawLine(uploadSpeedInfo,(LineChartView)findViewById(R.id.uploadSpeedchart),"uploadSpeed");
+                    drawLine(downloadSpeedInfo,(LineChartView)findViewById(R.id.downloadspeedchart),"downloadSpeed");
+                    drawLine(uploadTotalLengthInfo,(LineChartView)findViewById(R.id.uploadTotalLengthchart),"uploadTotalLength");
+                    drawLine(uploadTotalPkgInfo,(LineChartView)findViewById(R.id.uploadTotalPkgchart),"uploadTotalPkg");
+                    drawLine(downloadTotalLengthInfo,(LineChartView)findViewById(R.id.downloadTotalLengthchart),"downloadTotalLength");
+                    drawLine(downloadTotalPkgInfo,(LineChartView)findViewById(R.id.downloadTotalPkgchart),"downloadTotalPkg");
+//                    List<Line> lines = new ArrayList<Line>();
+//                    for (int i = 0; i < 1; ++i) {
+//
+//                        List<PointValue> values = new ArrayList<PointValue>();
+//                        for (int j = 0; j < INFOLENGTH; ++j) {
+//                            values.add(new PointValue(j,uploadSpeedInfo[j]));
+//                        }
+//
+//                        Line line = new Line(values);
+//                        line.setColor(ChartUtils.COLORS[i]);
+//                        line.setShape(ValueShape.CIRCLE);
+//                        line.setCubic(false);
+//                        line.setFilled(false);
+//                        line.setHasLabels(false);
+//                        line.setHasLabelsOnlyForSelected(false);
+//                        line.setHasLines(true);
+//                        line.setHasPoints(true);
+//                        if (true) {
+//                            line.setPointColor(ChartUtils.COLORS[(i + 1)
+//                                    % ChartUtils.COLORS.length]);
+//                        }
+//                        lines.add(line);
+//                    }
+//
+//
+//                    LineChartData data = new LineChartData(lines);
+//                    if (true) {
+//                        Axis axisX = new Axis();
+//                        Axis axisY = new Axis().setHasLines(true);
+//
+//                        if (true) {
+//                            axisX.setName("time X");
+//                            axisY.setName("uploadSpeed Y");
+//                        }
+//                        data.setAxisXBottom(axisX);
+//                        data.setAxisYLeft(axisY);
+//                    } else {
+//                        data.setAxisXBottom(null);
+//                        data.setAxisYLeft(null);
+//                    }
+//                    LineChartView uploadSpeedChart = (LineChartView)findViewById(R.id.uploadSpeedchart);
+//                    data.setBaseValue(Float.NEGATIVE_INFINITY);
+//                    uploadSpeedChart.setLineChartData(data);
+
+                    //第一条折线+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
                     //TODO 根据获得的上传、下载速度信息更新UI 更好看的界面  https://github.com/lecho/hellocharts-android
-                    uploadspeed.setText(uploadSpeed);
-                    downloadspeed.setText(downloadSpeed);
-                    uploadLength.setText(uploadTotalLength);
-                    downloadLength.setText(downloadTotalLength);
-                    uploadPkg.setText(uploadTotalPkg);
-                    downloadPkg.setText(downloadTotalPkg);
+//                    uploadspeed.setText(uploadSpeed);
+//                    downloadspeed.setText(downloadSpeed);
+//                    uploadLength.setText(uploadTotalLength);
+//                    downloadLength.setText(downloadTotalLength);
+//                    uploadPkg.setText(uploadTotalPkg);
+//                    downloadPkg.setText(downloadTotalPkg);
+                    Log.d("wjf","column over");
                     break;
                 case MSG_STATUS:
                     String status = (String)msg.obj;
@@ -143,6 +300,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       // chart = (ColumnChartView)findViewById( R.id.chart);
+        uploadSpeedInfo = new float[INFOLENGTH];
+        downloadSpeedInfo = new float[INFOLENGTH];
+        uploadTotalPkgInfo = new float[INFOLENGTH];
+        downloadTotalPkgInfo = new float[INFOLENGTH];
+        uploadTotalLengthInfo = new float[INFOLENGTH];
+        downloadTotalLengthInfo = new float[INFOLENGTH];
         // Example of a call to a native method
         //Done 检查网络状态，获取IPV6地址
         checkNetStatus();
@@ -152,6 +316,42 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view)
             {
                 Toast.makeText(MainActivity.this,"kai qi background",Toast.LENGTH_LONG).show();
+                //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                Runnable charrun = new Runnable() {
+                    @Override
+                    public void run() {
+
+                        int t = 1;
+                        while(t>0)
+                        {
+                            Message message = new Message();
+                            message.what = MSG_UPDATEUI;
+                            try{
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+
+                            String temp = new String(""+t+" "+t+" "+t+" "+t+" "+t+" "+t);
+                            Log.d("wjf","temp"+temp);
+                            message.obj = temp;
+
+                            mHandler.sendMessage(message);
+                            t++;
+                        }
+
+
+                    }
+                };
+
+
+                    Thread charthr = new Thread(charrun);
+                    charthr.start();
+
+
+
+
+                //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
                 Runnable background = new Runnable(){
                     public void run()
                     {
@@ -165,7 +365,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 };
                 Thread backgroundthr = new Thread(background);
-                backgroundthr.start();
+               // backgroundthr.start();
                 //---------------------------------------------------------------------------------------------------------------------------------------------
                 Runnable readIpPipe = new Runnable() {
                     @Override
@@ -214,6 +414,7 @@ public class MainActivity extends AppCompatActivity {
                                     }
                                 }
                                 in.close();
+
                             }catch (IOException e){
                                 e.printStackTrace();
                             }
@@ -223,7 +424,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 };
                 Thread readIpPipeth = new Thread(readIpPipe);
-                readIpPipeth.start();
+               // readIpPipeth.start();
             }
         });
         //TODO 开启前台计时器刷新界面
@@ -240,7 +441,52 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    private void drawLine(float infoArray[],LineChartView chartview,String title)
+    {
+        List<Line> lines = new ArrayList<Line>();
+        for (int i = 0; i < 1; ++i) {
 
+            List<PointValue> values = new ArrayList<PointValue>();
+            for (int j = 0; j < INFOLENGTH; ++j) {
+                values.add(new PointValue(j,infoArray[j]));
+            }
+
+            Line line = new Line(values);
+            line.setColor(ChartUtils.COLORS[i]);
+            line.setShape(ValueShape.CIRCLE);
+            line.setCubic(false);
+            line.setFilled(false);
+            line.setHasLabels(false);
+            line.setHasLabelsOnlyForSelected(false);
+            line.setHasLines(true);
+            line.setHasPoints(true);
+            if (true) {
+                line.setPointColor(ChartUtils.COLORS[(i + 1)
+                        % ChartUtils.COLORS.length]);
+            }
+            lines.add(line);
+        }
+
+
+        LineChartData data = new LineChartData(lines);
+        if (true) {
+            Axis axisX = new Axis();
+            Axis axisY = new Axis().setHasLines(true);
+
+            if (true) {
+                axisX.setName("time X");
+                axisY.setName(title+" Y");
+            }
+            data.setAxisXBottom(axisX);
+            data.setAxisYLeft(axisY);
+        } else {
+            data.setAxisXBottom(null);
+            data.setAxisYLeft(null);
+        }
+        LineChartView uploadSpeedChart = chartview;
+        data.setBaseValue(Float.NEGATIVE_INFINITY);
+        uploadSpeedChart.setLineChartData(data);
+    }
     private boolean isNetConnected() {
         ConnectivityManager connectivityManager =
                 (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
